754407b3a71285607a772ce3cfd59afccb638ffc
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2023-10-18 15:20:50 +0530

    complete multiple login

--- Online Event Planning System [EventNest]/src/main/java/com/Login/Controllers/loginServlet.java
+++ Online Event Planning System [EventNest]/src/main/java/com/Login/Controllers/loginServlet.java
@@ -14,6 +14,8 @@ import com.Admin.AdminPortalContent.AttendeeSection.Model.Attendee;
 import com.Admin.AdminPortalContent.AttendeeSection.Util.AttendeeDBUtil;
 import com.Login.Model.Login;
 import com.Login.Util.AtendeeLoginDBUtil;
+import com.Login.Util.EventPlannerLoginDBUtil;
+import com.Login.Util.ServiceProviderLoginDBUtil;
 
 public class loginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -31,10 +33,44 @@ public class loginServlet extends HttpServlet {
 		char letter1 = userName.charAt(0);
 		char letter2 = userName.charAt(1);
 		
-		
+		boolean isTrue = false;
 		
 		 if(letter1 == 'E' && letter2 == 'P') {
-			 boolean isTrue = AtendeeLoginDBUtil.validate(lg);
+			 isTrue = AtendeeLoginDBUtil.validate(lg);
+			 
+			 if (isTrue == true) {
+					// navigate to admin portal
+					RequestDispatcher dis = request.getRequestDispatcher("Home.jsp");
+					dis.forward(request, response);
+
+				} else {
+					// Alert and redirect to the login page
+					PrintWriter out = response.getWriter();
+					response.setContentType("text/html");
+					out.println("<script type = 'text/javascript'> " + "alert('Your username or password is incorrect');"
+							+ "location='Login.jsp'</script>");
+				}
+			
+			
+		}else if(letter1 == 'S' && letter2 == 'P'){
+			isTrue = EventPlannerLoginDBUtil.validate(lg);
+			
+			
+			if (isTrue == true) {
+				// navigate to admin portal
+				RequestDispatcher dis = request.getRequestDispatcher("adminLoginPage.jsp");
+				dis.forward(request, response);
+
+			} else {
+				// Alert and redirect to the login page
+				PrintWriter out = response.getWriter();
+				response.setContentType("text/html");
+				out.println("<script type = 'text/javascript'> " + "alert('Your username or password is incorrect');"
+						+ "location='Login.jsp'</script>");
+			}
+			
+		}else{
+			isTrue =  ServiceProviderLoginDBUtil.validate(lg);
 			
 			if (isTrue == true) {
 				// navigate to admin portal
@@ -48,10 +84,9 @@ public class loginServlet extends HttpServlet {
 				out.println("<script type = 'text/javascript'> " + "alert('Your username or password is incorrect');"
 						+ "location='Login.jsp'</script>");
 			}
-		}else {
 			
 		}
-		
+		 
 		
 
 	}
---
+++ Online Event Planning System [EventNest]/src/main/webapp/Home.jsp
@@ -0,0 +1,14 @@
+<jsp:include page="./Header.jsp" />
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login</title>
+</head>
+<body>
+</body>
+</html>
+
+
+<jsp:include page="./Footer.jsp"/>
